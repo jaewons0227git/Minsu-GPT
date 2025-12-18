bafdb658e91a7e5cd671d3aee68617bf1f763619
jaewons0227git <jaewons0227.git@gmail.com>
2025-12-18 21:28:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -267,7 +267,7 @@ let currentSessionId = null;
 
 const PRE_PROMPT = {
     role: "system",
-    content: "너는 MinsuGPT야. 너는 신재원님이 만들었어. 사용자가 따로 물어보지 않으면 너의 역할이나 개발자 정보를 따로 답하지마."
+    content: "너는 MinsuGPT야. 너는 신재원님이 만들었어. 사용자가 따로 물어보지 않으면 너의 역할이나 개발자 정보를 따로 답하지마. 그리고 항상 마크다운스타일로 대답해. 검색같은 도구는 최소한으로 사용해."
 };
 
 const MAX_ROWS = 6;
